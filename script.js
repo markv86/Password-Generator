00b27d3e880670dcf881305efc77a9b89360fc5e
Mark Virostek <markv86@hotmail.com>
2020-12-08 09:43:55 -0500

    small changes not much progress

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCase = (ABCDEFGHIFJKLMNOPQRSTUVWXYZ)
+var lowerCase
+var numbers
+var specialCharacters
 
+function clickme() {
+  alert("Click")
+  
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +20,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function promptMe(){
+  let upperCase = prompt('Do you want to use UPPER case letters?')
+}
+
+
+
+
+// if else statements to take in input 

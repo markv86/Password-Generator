10132f0893ac7a0ed8d3484085aa696aa62b6266
Mark Virostek <markv86@hotmail.com>
2020-12-08 12:44:23 -0500

    how many prompt box

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var upperCase = (ABCDEFGHIFJKLMNOPQRSTUVWXYZ)
-var lowerCase
-var numbers
-var specialCharacters
+// var generateBtn = document.querySelector("#generate");
+// var upperCase = (ABCDEFGHIFJKLMNOPQRSTUVWXYZ)
+// var lowerCase
+// var numbers
+// var specialCharacters
+
+
 
 function clickme() {
-  alert("Click")
+  alert("Are you ready to generate a new password?");
+  alert("ok then");
+  alert("Let do this!!!");
+  var howmany = prompt("How many digits will this undefeatable password contain? (8-128)");
+   console.log (howmany);
+     if (howmany <= 8 || howmany >= 128) {
+      alert ('Please try again');
+      prompt('You did not follow directions! Please choose a number between 8 - 128! ');
+     }
+     else {
+        prompt('Would you like to use UPPER case letter?');
+      }
+}   
+
   
-}
+
+ 
+  
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

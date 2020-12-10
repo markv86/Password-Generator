5cba209e0181fbbfb8d0ac47be2d65f6a3116bcd
Mark Virostek <markv86@hotmail.com>
2020-12-09 19:44:41 -0500

    if statements for concat strings console.log okay

--- script.js
+++ script.js
@@ -3,10 +3,7 @@
 
 // Choices for password generation
 
-var upperSelection = ['A','B','C','D','E','F','G','H','I','F','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-var lowerSelection = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var numbers = ['0','1','2','3','4','5','6','7','8','9']
-var specialSelection = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+
 
 
 //   PROMPTS    
@@ -24,42 +21,45 @@ function clickme() {
     }
 
     var upperCase = confirm('Would you like to use UPPER case letters"');
-    console.log(upperCase)
+    // console.log(upperCase)
     
     var lowerCase = confirm('Would you like to use lower case letters?');  
-    console.log(lowerCase)
+    // console.log(lowerCase)
    
     var specialCharacters = confirm('Would you like to use $P3C!@| Characters?');
-    console.log(specialCharacters)
+    // console.log(specialCharacters)
     
     var integers = confirm('Would you like to use numbers?');
-    console.log(integers)
+    // console.log(integers)
     
-  }
+
+    var upperSelection = ['A','B','C','D','E','F','G','H','I','F','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    var lowerSelection = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    var numbers = ['0','1','2','3','4','5','6','7','8','9']
+    var specialSelection = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+    var chosenCharacter = []
 //     END OF PROMPTS
   
 //     Push options to an array
 
+
   if(upperCase){
-    passwordGenerateArray.concat(upperSelection)
+    console.log(upperSelection)
   }
-  if(lowerSelection){
-    passwordGenerateArray.concat(lowerSelection)
+  if(lowerCase){
+    console.log(lowerSelection)
   }
-  if(specialSelection){
-    passwordGenerateArray.concat(specialSelection)
-  }  
   if(integers){
-    passwordGenerateArray.concat(integers)
+    console.log(numbers)
+  }
+  if(specialCharacters){
+    console.log(specialSelection)
   }
 
+//  concat final string
 
-
-passwordGenerateArray = []
-console.log(passwordGenerateArray)
-
-
-
+var passwordGeneratephrase = upperSelection.concat(lowerSelection, numbers, specialSelection)
+  console.log(passwordGeneratephrase)
 
 
 
@@ -70,6 +70,9 @@ console.log(passwordGenerateArray)
 
 
 
+
+
+}
 // // Write password to the #password input
 // function writePassword() {
 //   var password = generatePassword();
